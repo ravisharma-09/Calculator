108f52ea2f317754a10462efa43f4b978a46617d
ravisharma-09 <sharmaravi75080@gmail.com>
2026-05-06 10:29:24 +0530

    responsive + darkmode

--- main.js
+++ main.js
@@ -31,3 +31,18 @@ function clearDisplay() {
 function backspace(){
     display.value = display.value.slice(0,-1) ;
 }
+
+
+const themebtn = document.getElementById("theme") ;
+
+
+themebtn.addEventListener("click", () =>{
+    document.body.classList.toggle("dark") ;
+    
+    if (document.body.classList.contains("dark")){
+        themebtn.textContent = "light-mode"
+    }else{
+        themebtn.textContent = "dark-mode"
+    }
+}) ;
+
