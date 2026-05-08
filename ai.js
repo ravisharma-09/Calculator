f1e298eb4235ceba7279d18a3a4a5c9e63434c15
ravisharma-09 <sharmaravi75080@gmail.com>
2026-05-09 01:07:20 +0530

    done with text to calculation

--- ai.js
+++ ai.js
@@ -14,3 +14,44 @@ themebtn.addEventListener("click", () => {
     }
 
 });
+
+function parseInput(input){
+    return input
+        replace(/what is/g, "")
+        .replace(/calculate/g, "")
+        .replace(/how much is/g, "")
+        .replace(/please/g, "")
+        .replace(/plus/g, "+")
+        .replace(/minus/g, "-")
+
+        .replace(/multiply/g, "*")
+        .replace(/times/g, "*")
+
+        .replace(/divided by/g, "/")
+
+
+        .replace(/percent of/g, "*0.01*")
+
+        .replace(/square root of/g, "sqrt")
+
+        .trim();
+
+}
+
+async function solveAi() {
+
+    const input = document.getElementById("ai-input").value ;
+    const expression = parseInput(input) ;
+
+    try{
+        const response = await fetch(
+            `https://api.mathjs.org/v4/?expr=${encodeURIComponent(expression)}`
+        ) ;
+        const data = await response.text() ;
+        result.value = data ;
+    }
+    catch(error){
+        result.value = "ERROR" ;
+    }
+}
+
